ac915bb150a0ac218c540bbde1bf489797bb43b6
Torsten Werner <torsten.werner@westernacher.com>
2015-03-02 20:06:47 +0100

    add atomic counter

--- src/main/web.go
+++ src/main/web.go
@@ -6,17 +6,33 @@ import (
     "net/http"
 )
 
-type Hello struct{}
-
-func (h Hello) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello, 世界")
-}
-
 func main() {
-    var h Hello
+    ch := make(chan int, 3)
+    go count(ch)
+    h := Hello{ch}
     fmt.Println("application started")
     err := http.ListenAndServe("0.0.0.0:4000", h)
     if err != nil {
         log.Fatal(err)
+    }
+}
+
+type Hello struct {
+    ch <-chan int
+}
+
+func (h Hello) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "%d: Hello, 世界", h.getCounter())
+}
+
+func (h Hello) getCounter() int {
+    return <-h.ch
+}
+
+func count(ch chan <- int) {
+    counter := 0
+    for {
+        ch <- counter
+        counter ++
     }
 }
